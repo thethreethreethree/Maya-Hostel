576e66db69cf414256aaf54572de76c0c52b1d7d
WDT <johnsyramos@gmail.com>
2026-05-09 07:30:44 +0800

    Delete maya-hostel directory

--- maya-hostel/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".navbar");
-
-  if(window.scrollY > 50){
-    navbar.style.background = "rgba(0,0,0,0.75)";
-  } else {
-    navbar.style.background = "rgba(0,0,0,0.2)";
-  }
-});
